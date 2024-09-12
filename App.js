f72597d6f5293985b43560e8c44bcfa5767dc3b2
Karan-Bharti <90687247+Karan-Bharti@users.noreply.github.com>
2024-09-12 15:01:11 +0530

    React beginning

---
+++ App.js
@@ -0,0 +1,5 @@
+const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{id:"heading"},"I am H1 tag"),React.createElement("h2",{id:"heading"},"I am H2 tag")]))
+
+
+const root=ReactDOM.createRoot(document.getElementById("root"))
+root.render(parent)
