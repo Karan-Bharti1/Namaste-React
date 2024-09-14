a5f62864cce77ce944c2430e014c2749697ef44b
Karan-Bharti <90687247+Karan-Bharti@users.noreply.github.com>
2024-09-14 11:21:49 +0530

    Parcel and npm installation

--- App.js
+++ App.js
@@ -1,3 +1,5 @@
+import React from "react"
+import ReactDOM from 'react-dom/client'
 const parent=React.createElement("div",{id:"parent"},React.createElement("div",{id:"child"},[React.createElement("h1",{id:"heading"},"I am H1 tag"),React.createElement("h2",{id:"heading"},"I am H2 tag")]))
 
 
