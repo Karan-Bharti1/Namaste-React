696115dd4ae340153423f839cc0e1493170a1d5b
Karan-Bharti <90687247+Karan-Bharti@users.noreply.github.com>
2024-09-16 11:38:30 +0530

    Starting with building food ordering App

--- App.js
+++ App.js
@@ -1,19 +1,394 @@
 import React from "react"
 import ReactDOM from 'react-dom/client'
-const number=1000
-const elem=<span>You Got it</span>
-const Title=()=>{
-    return (<h1> {number}
-        React Using JSX {elem}
-        </h1>)
-}
-const HeadingFunctionalComponent=()=>(
-    <div>
-        <Title/>
-     <h1>Heading Functional Component </h1>
+const restObj= [
+    {
+      "info": {
+        "id": "718482",
+        "name": "Chinese Wok",
+        "cloudinaryImageId": "e0839ff574213e6f35b3899ebf1fc597",
+        "locality": "Phase 10",
+        "areaName": "Phase 10",
+        "costForTwo": "₹250 for two",
+        "cuisines": [
+          "Chinese",
+          "Asian",
+          "Tibetan",
+          "Desserts"
+        ],
+        "avgRating": 4.3,
+        "parentId": "61955",
+        "avgRatingString": "4.3",
+        "totalRatingsString": "960",
+        "sla": {
+          "deliveryTime": 47,
+          "lastMileTravel": 8,
+          "serviceability": "SERVICEABLE",
+          "slaString": "45-50 mins",
+          "lastMileTravelString": "8.0 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-09-16 00:00:00",
+          "opened": true
+        },
+        "badges": {
+          
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "ITEMS",
+          "subHeader": "AT ₹199"
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        },
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "3.1",
+            "ratingCount": "45"
+          },
+          "source": "GOOGLE",
+          "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      "analytics": {
+        "context": "seo-data-00f8530d-e07b-4c4a-87d7-62192e4a1d55"
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/city/chandigarh/chinese-wok-phase-10-rest718482",
+        "text": "RESTAURANT_MENU",
+        "type": "WEBLINK"
+      },
+      "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+      "info": {
+        "id": "41350",
+        "name": "Burger King",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/11/55d0daa1-ad2e-4893-be06-4709c5c68d49_41350.JPG",
+        "locality": "Sector 35",
+        "areaName": "Sector 35",
+        "costForTwo": "₹400 for two",
+        "cuisines": [
+          "Burgers",
+          "American"
+        ],
+        "avgRating": 4.4,
+        "parentId": "166",
+        "avgRatingString": "4.4",
+        "totalRatingsString": "54K+",
+        "sla": {
+          "deliveryTime": 25,
+          "lastMileTravel": 3.6,
+          "serviceability": "SERVICEABLE",
+          "slaString": "20-25 mins",
+          "lastMileTravelString": "3.6 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-09-16 05:29:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "Green%20Dot%20Awards/Best%20In%20Veg%20Burger.png",
+              "description": "Delivery!"
+            },
+            {
+              "imageId": "Rxawards/_CATEGORY-Burger.png",
+              "description": "Delivery!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "Delivery!",
+                    "imageId": "Green%20Dot%20Awards/Best%20In%20Veg%20Burger.png"
+                  }
+                },
+                {
+                  "attributes": {
+                    "description": "Delivery!",
+                    "imageId": "Rxawards/_CATEGORY-Burger.png"
+                  }
+                }
+              ]
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "50% OFF",
+          "subHeader": "UPTO ₹100"
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        },
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "4.3",
+            "ratingCount": "4.3K+"
+          },
+          "source": "GOOGLE",
+          "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      "analytics": {
+        "context": "seo-data-00f8530d-e07b-4c4a-87d7-62192e4a1d55"
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/city/chandigarh/burger-king-sector-35-rest41350",
+        "text": "RESTAURANT_MENU",
+        "type": "WEBLINK"
+      },
+      "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    },
+    {
+      "info": {
+        "id": "42802",
+        "name": "Subway",
+        "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/6/4/7693df43-93dc-4347-abbc-a813c9678521_42802.JPG",
+        "areaName": "Sector 19",
+        "costForTwo": "₹350 for two",
+        "cuisines": [
+          "Fast Food",
+          "Healthy Food",
+          "Salads",
+          "Snacks",
+          "Desserts",
+          "Beverages"
+        ],
+        "avgRating": 4.4,
+        "parentId": "2",
+        "avgRatingString": "4.4",
+        "totalRatingsString": "5.6K+",
+        "sla": {
+          "deliveryTime": 16,
+          "lastMileTravel": 0.6,
+          "serviceability": "SERVICEABLE",
+          "slaString": "15-20 mins",
+          "lastMileTravelString": "0.6 km",
+          "iconType": "ICON_TYPE_EMPTY"
+        },
+        "availability": {
+          "nextCloseTime": "2024-09-15 23:58:00",
+          "opened": true
+        },
+        "badges": {
+          "imageBadges": [
+            {
+              "imageId": "Rxawards/_CATEGORY-Sandwiches.png",
+              "description": "Delivery!"
+            }
+          ]
+        },
+        "isOpen": true,
+        "type": "F",
+        "badgesV2": {
+          "entityBadges": {
+            "imageBased": {
+              "badgeObject": [
+                {
+                  "attributes": {
+                    "description": "Delivery!",
+                    "imageId": "Rxawards/_CATEGORY-Sandwiches.png"
+                  }
+                }
+              ]
+            },
+            "textBased": {
+              
+            },
+            "textExtendedBadges": {
+              
+            }
+          }
+        },
+        "aggregatedDiscountInfoV3": {
+          "header": "₹150 OFF",
+          "subHeader": "ABOVE ₹299",
+          "discountTag": "FLAT DEAL"
+        },
+        "orderabilityCommunication": {
+          "title": {
+            
+          },
+          "subTitle": {
+            
+          },
+          "message": {
+            
+          },
+          "customIcon": {
+            
+          }
+        },
+        "differentiatedUi": {
+          "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+          "differentiatedUiMediaDetails": {
+            "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+            "lottie": {
+              
+            },
+            "video": {
+              
+            }
+          }
+        },
+        "reviewsSummary": {
+          
+        },
+        "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+        "restaurantOfferPresentationInfo": {
+          
+        },
+        "externalRatings": {
+          "aggregatedRating": {
+            "rating": "--"
+          }
+        },
+        "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+      },
+      "analytics": {
+        "context": "seo-data-00f8530d-e07b-4c4a-87d7-62192e4a1d55"
+      },
+      "cta": {
+        "link": "https://www.swiggy.com/city/chandigarh/subway-sector-19-rest42802",
+        "text": "RESTAURANT_MENU",
+        "type": "WEBLINK"
+      },
+      "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+    }]
+const Header=()=>{
+    return <div className="header">
+<div className="logo-container">
+    <img className="logo" src="https://i.etsystatic.com/22467704/r/il/cc9dd4/2649320480/il_570xN.2649320480_9w9j.jpg"/>
+</div>
+<div className="nav-items">
+<ul>
+    <li>Home</li>
+    <li>About Us</li>
+    <li>Contact Us</li>
+    <li>Cart</li>
+</ul>
+</div>
     </div>
-)
-const jsxHeading=<h1 className="heading">H1 using Jsx</h1>;// ReactElement using JSX
-const root = ReactDOM.createRoot(document.getElementById("root"))
+}
+const RestaurantCard=({restData})=>{ 
+  const {name,cuisines,avgRating,costForTwo}=restData?.info
+   return <div className="restaurant-card">
+    <img className="restaurant-image" src={"https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/"+restData.info.cloudinaryImageId}/>
+<h3 className="restaurant-name">{name}</h3>
+<h5>{cuisines.join(" , ")}</h5>
+<h5>{avgRating} stars</h5>
+<h5>{costForTwo}</h5>
+    </div>
+    }
 
-root.render(<HeadingFunctionalComponent/>)
+
+const Body=()=>{
+return <div className="body">
+<div className="search">Search</div>
+<div className="res-container">
+{restObj.map(res => 
+  <RestaurantCard key={res.info.id}restData={res}/>)}
+</div>
+
+</div>
+}
+const AppLayout=()=>{
+    return <div className="app">
+<Header/>
+<Body/>
+    </div>
+}
+ const root=ReactDOM.createRoot(document.getElementById("root"))
+ root.render(<AppLayout/>)
