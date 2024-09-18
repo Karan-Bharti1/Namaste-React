dd6f9dbba18bb2cd7e4405b5531b020a32aa7ce1
Karan-Bharti <90687247+Karan-Bharti@users.noreply.github.com>
2024-09-18 23:07:27 +0530

    Data fetch and updating it using useEffect and installing a Login-Logout Button using useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,16 +1,24 @@
 import RestaurantCard from "./RestaurantCard";
-import restObj from "../../utils/mockdata";
-import { useState } from "react";
+import Shimmer from "./Shimmer";
+import { useState,useEffect } from "react";
 
 const Body=()=>{
-  const [listOfRestaurants,setListOfRestaurants]=useState(restObj)
-    return <div className="body">
+  const [listOfRestaurants,setListOfRestaurants]=useState([])
+  useEffect(()=>{fetchData()},[])
+  const  fetchData= async()=>{
+const data =  await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.73390&lng=76.78890&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
+const jsonData= await data.json()
+
+setListOfRestaurants(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+  }
+
+
+return listOfRestaurants.length === 0?<Shimmer/>: <div className="body">
     <div className="filter">
 <button className="filter-btn" onClick={()=>{
-  const filteredData=listOfRestaurants.filter((res)=>res.info.avgRating>4)
+  const filteredData=listOfRestaurants.filter((res)=>res.info.avgRating>4.3)
   setListOfRestaurants(filteredData)
 }}>Top Rated Restaurant</button>
-
     </div>
     <div className="res-container">
     {listOfRestaurants.map(res => 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import {Logo_URL} from "../../utils/constants";
-
+import { useState } from "react";
  export const Header=()=>{
+    const [btnName,setBtnName]=useState("Login")
     return <div className="header">
 <div className="logo-container">
     <img className="logo" src={Logo_URL}/>
@@ -11,6 +12,9 @@ import {Logo_URL} from "../../utils/constants";
     <li>About Us</li>
     <li>Contact Us</li>
     <li>Cart</li>
+    <button onClick={()=>{
+      btnName==="Login" ? setBtnName("Log Out"):setBtnName("Login")
+    }} className="login-btn">{btnName} 👤</button>
 </ul>
 </div>
     </div>
