c658660e3932cdaf48815f53f73c45210f749279
Karan-Bharti <90687247+Karan-Bharti@users.noreply.github.com>
2024-09-19 14:20:25 +0530

    Added Search Bar

--- src/components/Body.js
+++ src/components/Body.js
@@ -4,24 +4,37 @@ import { useState,useEffect } from "react";
 
 const Body=()=>{
   const [listOfRestaurants,setListOfRestaurants]=useState([])
+  const [filteredData,setFilteredData]=useState([])
+  const [searchText,setSearchText]=useState("")
   useEffect(()=>{fetchData()},[])
   const  fetchData= async()=>{
 const data =  await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=30.73390&lng=76.78890&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING")
 const jsonData= await data.json()
 
 setListOfRestaurants(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
+setFilteredData(jsonData?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
   }
 
 
-return listOfRestaurants.length === 0?<Shimmer/>: <div className="body">
+return filteredData.length === 0?<Shimmer/>: <div className="body">
+
     <div className="filter">
+    <div className="search">
+    <input type="text" className="search-box" value={searchText} onChange={(e)=>setSearchText(e.target.value)}/>
+    <button className="search-btn" onClick={()=>{
+      const filterData=listOfRestaurants.filter((res)=>{return res.info.name.toLowerCase().includes(searchText.toLowerCase())
+
+      })
+      setFilteredData(filterData)
+    }}>Search</button>
+  </div>
 <button className="filter-btn" onClick={()=>{
   const filteredData=listOfRestaurants.filter((res)=>res.info.avgRating>4.3)
-  setListOfRestaurants(filteredData)
-}}>Top Rated Restaurant</button>
+  setFilteredData(filteredData)
+}}>⭐ Rated </button>
     </div>
     <div className="res-container">
-    {listOfRestaurants.map(res => 
+    {filteredData.map(res => 
       <RestaurantCard key={res.info.id} restData={res}/>)}
     </div>
     
